3fd7805718a35419b140423777dded8fbd9bb694
Libert Brice <b.libert@ideematic.com>
2026-03-16 13:40:29 +0100

    add colors constant

---
+++ contants/colors.ts
@@ -0,0 +1,26 @@
+export const Colors = {
+  primary: '#CC0000',
+  primaryDark: '#990000',
+  primaryLight: '#FF3333',
+
+  critical: '#FF0000',
+  warning: '#FF9500',
+  stable: '#34C759',
+  unknown: '#8E8E93',
+
+  airway: '#FF6B6B',
+  breathing: '#4ECDC4',
+  circulation: '#FFE66D',
+  disability: '#A8E6CF',
+  exposure: '#DDA0DD',
+
+  background: '#F2F2F7',
+  card: '#FFFFFF',
+  text: '#1C1C1E',
+  textSecondary: '#6C6C70',
+  border: '#C6C6C8',
+
+  actionOui: '#34C759',
+  actionNon: '#FF3B30',
+  actionNeutre: '#007AFF',
+}
